d338ce3c4f6e3b77969ff9689f6c928763609d8c
Ethan Li <ethan.jeffrey@gmail.com>
2020-12-14 02:07:35 +0000

    cleaned up files and updated install script to different name and should run well now. Changes bashrc and runs it to update the path.

--- robotDropBox/src/load_box.sh
+++
@@ -1,1 +0,0 @@
-rosrun gazebo_ros spawn_model -file boxurdf.urdf -urdf -model $1 -y $2 -x $3 -z 5.0
--- robotDropBox/src/removeBox.sh
+++
@@ -1,5 +0,0 @@
-for i in {0..255}
-do
-    rosservice call gazebo/delete_model box$i &
-    echo $i
-done
